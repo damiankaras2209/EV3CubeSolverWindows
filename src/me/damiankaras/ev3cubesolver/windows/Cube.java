fdd23b927e2dfe1100d6305ec8ef01e2267bcd08
Damian Karaś <damiankaras2209@gmail.com>
2022-08-20 13:59:50 +0200

    some simplifying

--- src/me/damiankaras/ev3cubesolver/windows/Cube.java
+++ src/me/damiankaras/ev3cubesolver/windows/Cube.java
@@ -1,13 +1,20 @@
 package me.damiankaras.ev3cubesolver.windows;
 
 import me.damiankaras.ev3cubesolver.windows.GUI.Renderer;
-
 import java.awt.*;
 
 public class Cube {
 
+    private static final String colorChars = "WOYRGB-";
+    private static final short[] rotateDisplayZ = {15,12,9,16,13,10,17,14,11,33,30,27,34,31,28,35,32,29,24,21,18,25,22,19,26,23,20,42,39,36,43,40,37,44,41,38,6,3,0,7,4,1,8,5,2,47,50,53,46,49,52,45,48,51};
+    private static final short[] rotateDisplayY_CW = {2,5,8,1,4,7,0,3,6,45,46,47,48,49,50,51,52,53,9,10,11,12,13,14,15,16,17,33,30,27,34,31,28,35,32,29,18,19,20,21,22,23,24,25,26,36,37,38,39,40,41,42,43,44};
+    private static final short[] rotateDisplayY_CCW = {6,3,0,7,4,1,8,5,2,18,19,20,21,22,23,24,25,26,36,37,38,39,40,41,42,43,44,29,32,35,28,31,34,27,30,33,45,46,47,48,49,50,51,52,53,9,10,11,12,13,14,15,16,17};
+    private static final short[] rotateFacesDisplayZ = {3,0,2,5,4,1};
+    private static final short[] rotateFacesDisplayY_CW = {0,2,3,4,1,5};
+    private static final short[] rotateFacesDisplayY_CCW = {0,4,1,2,3,5};
+
     private enum StickerColor {
-        white(255,255,255), orange(255,128,0),  yellow(255,255,0), red(255,0,0), green(0,255,0),  blue(0,0,255);
+        white(255,255,255), orange(255,128,0),  yellow(255,255,0), red(255,0,0), green(0,255,0),  blue(0,0,255), gray(128, 128, 128);
 
         private Color color;
 
@@ -23,50 +30,44 @@ public class Cube {
     private static final int CCW = 1;
 
     public static Color[][][] cube;
-    private String display;
-    private String faces;
-    int counter;
+    private static boolean staticOrientation = true;
+    private String display = "";
+    private String faces = "ULFRBD";
+    private String fixedDisplay = "";
+    private String fixedFaces = "";
 
-    me.damiankaras.ev3cubesolver.windows.GUI.Renderer renderer;
+    private me.damiankaras.ev3cubesolver.windows.GUI.Renderer renderer;
 
-    public Cube() {
+    Cube() {
         cube = new Color[6][3][3];
         for (int i=0;i<6;i++) {
             for (int j=0;j<3;j++) {
                 for (int k = 0; k < 3; k++) {
-//                    cube[i][j][k] = StickerColor.values()[i].getStickerColor();
-                    cube[i][j][k] = Color.GRAY;
+                    cube[i][j][k] = StickerColor.gray.getStickerColor();
                 }
             }
         }
     }
 
-    Color getColor(Character c) {
-        switch (c) {
-            case 'W': return StickerColor.values()[0].getStickerColor();
-            case 'O': return StickerColor.values()[1].getStickerColor();
-            case 'Y': return StickerColor.values()[2].getStickerColor();
-            case 'R': return StickerColor.values()[3].getStickerColor();
-            case 'G': return StickerColor.values()[4].getStickerColor();
-            case 'B': return StickerColor.values()[5].getStickerColor();
-            default: return Color.GRAY;
-        }
-
-    }
 
     void update(String str) {
 
-        if(true) {
-            fixDisplay(str);
-            str = display;
-        }
+        System.out.println(str);
 
+        display = str.substring(0, 54);
+        fixedDisplay = display;
+        faces = str.substring(55);
+        fixedFaces = faces;
+
+        if(staticOrientation) {
+            fixOrientation();
+            str = fixedDisplay;
+        }
 
         for (int i=0; i<6; i++) {
             for (int j=0; j<3; j++) {
                 for (int k=0; k<3; k++) {
-//                    cube[i][j][k] = StickerColor.values()[i].getStickerColor();
-                    cube[i][j][k] = getColor(str.charAt(i*9 + j*3 + k));
+                    cube[i][j][k] = StickerColor.values()[colorChars.indexOf(str.charAt(i*9 + j*3 + k))].getStickerColor();
                 }
             }
         }
@@ -78,217 +79,53 @@ public class Cube {
         renderer = r;
     }
 
-    private void fixDisplay(String str) {
-
-        display = str.substring(0, 54);
-        faces = str.substring(55);
-
-//        StringBuilder builder = new StringBuilder(6);
-//        builder.append(display.charAt(0*9+4));
-//        builder.append(display.charAt(4*9+4));
-//        builder.append(display.charAt(2*9+4));
-//        builder.append(display.charAt(1*9+4));
-//        builder.append(display.charAt(5*9+4));
-//        builder.append(display.charAt(3*9+4));
-//        faces = builder.toString();
-
-        int x = faces.indexOf("D");
-//        int x = faces.indexOf("Y");
+    private void fixOrientation() {
+        
+        int x = fixedFaces.indexOf("D");
         switch (x) {
-            case 0:
-                rotateZ();
-                rotateZ();
-                break;
-            case 1:
-                rotateZ();
-                break;
-            case 2:
-                rotateY(CW, 1);
-                rotateZ();
-                break;
-            case 3:
-                rotateY(CW, 2);
-                rotateZ();
-                break;
-            case 4:
-                rotateY(CCW, 1);
-                rotateZ();
-                break;
+            case 0: rotateZ(2); break;
+            case 1: rotateZ(1); break;
+            case 2: rotateY(CW, 1); rotateZ(1); break;
+            case 3: rotateY(CW, 2); rotateZ(1); break;
+            case 4: rotateY(CCW, 1); rotateZ(1); break;
         }
 
-        x = faces.indexOf("F");
-//        x = faces.indexOf("B");
+        x = fixedFaces.indexOf("F");
         switch (x) {
-            case 1:
-                rotateY(CCW, 1);
-                break;
-            case 3:
-                rotateY(CW, 1);
-                break;
-            case 4:
-                rotateY(CW, 2);
-                break;
+            case 1: rotateY(CCW, 1); break;
+            case 3: rotateY(CW, 1); break;
+            case 4: rotateY(CW, 2); break;
         }
 
     }
 
     private void rotateY(int dir, int iterations) {
 
-        StringBuilder stringBuilder = new StringBuilder(display);
-
         for(int j=0; j<iterations; j++) {
-            StringBuilder facesBuilder = new StringBuilder(faces);
-            if (dir == 0) {
-                facesBuilder.setCharAt(1, faces.charAt(2));
-                facesBuilder.setCharAt(2, faces.charAt(3));
-                facesBuilder.setCharAt(3, faces.charAt(4));
-                facesBuilder.setCharAt(4, faces.charAt(1));
-            } else {
-                facesBuilder.setCharAt(2, faces.charAt(1));
-                facesBuilder.setCharAt(3, faces.charAt(2));
-                facesBuilder.setCharAt(4, faces.charAt(3));
-                facesBuilder.setCharAt(1, faces.charAt(4));
-            }
-            faces = facesBuilder.toString();
+            StringBuilder facesBuilder = new StringBuilder(6);
+            for(int i=0; i<6; i++)
+                facesBuilder.append(fixedFaces.charAt(dir == CW ? rotateFacesDisplayY_CW[i] : rotateFacesDisplayY_CCW[i]));
+            fixedFaces = facesBuilder.toString();
 
-            //if(!isScanning) {
-
-            if (dir == 0) {
-                stringBuilder.setCharAt(0, display.charAt(2));
-                stringBuilder.setCharAt(1, display.charAt(5));
-                stringBuilder.setCharAt(2, display.charAt(8));
-                stringBuilder.setCharAt(3, display.charAt(1));
-                stringBuilder.setCharAt(5, display.charAt(7));
-                stringBuilder.setCharAt(6, display.charAt(0));
-                stringBuilder.setCharAt(7, display.charAt(3));
-                stringBuilder.setCharAt(8, display.charAt(6));
-
-                stringBuilder.setCharAt(3 * 9 + 2, display.charAt(3 * 9 + 0));
-                stringBuilder.setCharAt(3 * 9 + 5, display.charAt(3 * 9 + 1));
-                stringBuilder.setCharAt(3 * 9 + 8, display.charAt(3 * 9 + 2));
-                stringBuilder.setCharAt(3 * 9 + 1, display.charAt(3 * 9 + 3));
-                stringBuilder.setCharAt(3 * 9 + 7, display.charAt(3 * 9 + 5));
-                stringBuilder.setCharAt(3 * 9 + 0, display.charAt(3 * 9 + 6));
-                stringBuilder.setCharAt(3 * 9 + 3, display.charAt(3 * 9 + 7));
-                stringBuilder.setCharAt(3 * 9 + 6, display.charAt(3 * 9 + 8));
-
-                for (int i = 0; i < 9; i++) {
-                    stringBuilder.setCharAt(2 * 9 + i, display.charAt(1 * 9 + i));
-                    stringBuilder.setCharAt(1 * 9 + i, display.charAt(5 * 9 + i));
-                    stringBuilder.setCharAt(5 * 9 + i, display.charAt(4 * 9 + i));
-                    stringBuilder.setCharAt(4 * 9 + i, display.charAt(2 * 9 + i));
-                }
-
-            } else {
-                stringBuilder.setCharAt(2, display.charAt(0));
-                stringBuilder.setCharAt(5, display.charAt(1));
-                stringBuilder.setCharAt(8, display.charAt(2));
-                stringBuilder.setCharAt(1, display.charAt(3));
-                stringBuilder.setCharAt(7, display.charAt(5));
-                stringBuilder.setCharAt(0, display.charAt(6));
-                stringBuilder.setCharAt(3, display.charAt(7));
-                stringBuilder.setCharAt(6, display.charAt(8));
-
-                stringBuilder.setCharAt(3 * 9 + 0, display.charAt(3 * 9 + 2));
-                stringBuilder.setCharAt(3 * 9 + 1, display.charAt(3 * 9 + 5));
-                stringBuilder.setCharAt(3 * 9 + 2, display.charAt(3 * 9 + 8));
-                stringBuilder.setCharAt(3 * 9 + 3, display.charAt(3 * 9 + 1));
-                stringBuilder.setCharAt(3 * 9 + 5, display.charAt(3 * 9 + 7));
-                stringBuilder.setCharAt(3 * 9 + 6, display.charAt(3 * 9 + 0));
-                stringBuilder.setCharAt(3 * 9 + 7, display.charAt(3 * 9 + 3));
-                stringBuilder.setCharAt(3 * 9 + 8, display.charAt(3 * 9 + 6));
-
-                for (int i = 0; i < 9; i++) {
-                    stringBuilder.setCharAt(1 * 9 + i, display.charAt(2 * 9 + i));
-                    stringBuilder.setCharAt(5 * 9 + i, display.charAt(1 * 9 + i));
-                    stringBuilder.setCharAt(4 * 9 + i, display.charAt(5 * 9 + i));
-                    stringBuilder.setCharAt(2 * 9 + i, display.charAt(4 * 9 + i));
-                }
-            }
-            //}
-            display = stringBuilder.toString();
+            StringBuilder displayBuilder = new StringBuilder(54);
+            for(int i=0; i<54; i++)
+                displayBuilder.append(fixedDisplay.charAt(dir == CW ? rotateDisplayY_CW[i] : rotateDisplayY_CCW[i]));
+            fixedDisplay = displayBuilder.toString();
         }
     }
 
-    private void rotateZ() {
+    private void rotateZ(int iterations) {
+        for(int j=0; j<iterations; j++) {
+            StringBuilder facesBuilder = new StringBuilder(6);
+            for(int i=0; i<6; i++)
+                facesBuilder.append(fixedFaces.charAt(rotateFacesDisplayZ[i]));
+            fixedFaces = facesBuilder.toString();
 
-        StringBuilder  facesBuilder = new StringBuilder(faces);
-        facesBuilder.setCharAt(0, faces.charAt(3));
-        facesBuilder.setCharAt(1, faces.charAt(0));
-        facesBuilder.setCharAt(3, faces.charAt(5));
-        facesBuilder.setCharAt(5, faces.charAt(1));
+            StringBuilder stringBuilder = new StringBuilder(54);
+            for (int i = 0; i < 54; i++)
+                stringBuilder.append(fixedDisplay.charAt(rotateDisplayZ[i]));
+            fixedDisplay = stringBuilder.toString();
+        }
 
-        faces = facesBuilder.toString();
-        System.out.println(faces);
-
-        // if(!isScanning) {
-        StringBuilder stringBuilder = new StringBuilder(display);
-
-        stringBuilder.setCharAt(2*9 + 2, display.charAt(2*9 + 0));
-        stringBuilder.setCharAt(2*9 + 5, display.charAt(2*9 + 1));
-        stringBuilder.setCharAt(2*9 + 8, display.charAt(2*9 + 2));
-        stringBuilder.setCharAt(2*9 + 1, display.charAt(2*9 + 3));
-        stringBuilder.setCharAt(2*9 + 7, display.charAt(2*9 + 5));
-        stringBuilder.setCharAt(2*9 + 0, display.charAt(2*9 + 6));
-        stringBuilder.setCharAt(2*9 + 3, display.charAt(2*9 + 7));
-        stringBuilder.setCharAt(2*9 + 6, display.charAt(2*9 + 8));
-
-        stringBuilder.setCharAt(5*9 + 0, display.charAt(5*9 + 2));
-        stringBuilder.setCharAt(5*9 + 1, display.charAt(5*9 + 5));
-        stringBuilder.setCharAt(5*9 + 2, display.charAt(5*9 + 8));
-        stringBuilder.setCharAt(5*9 + 3, display.charAt(5*9 + 1));
-        stringBuilder.setCharAt(5*9 + 5, display.charAt(5*9 + 7));
-        stringBuilder.setCharAt(5*9 + 6, display.charAt(5*9 + 0));
-        stringBuilder.setCharAt(5*9 + 7, display.charAt(5*9 + 3));
-        stringBuilder.setCharAt(5*9 + 8, display.charAt(5*9 + 6));
-
-        stringBuilder.setCharAt( 0, display.charAt(9 + 6));
-        stringBuilder.setCharAt( 1, display.charAt(9 + 3));
-        stringBuilder.setCharAt( 2, display.charAt(9 + 0));
-        stringBuilder.setCharAt( 3, display.charAt(9 + 7));
-        stringBuilder.setCharAt( 4, display.charAt(9 + 4));
-        stringBuilder.setCharAt( 5, display.charAt(9 + 1));
-        stringBuilder.setCharAt( 6, display.charAt(9 + 8));
-        stringBuilder.setCharAt( 7, display.charAt(9 + 5));
-        stringBuilder.setCharAt( 8, display.charAt(9 + 2));
-
-        stringBuilder.setCharAt( 9 + 0, display.charAt(3*9 + 6));
-        stringBuilder.setCharAt( 9 + 1, display.charAt(3*9 + 3));
-        stringBuilder.setCharAt( 9 + 2, display.charAt(3*9 + 0));
-        stringBuilder.setCharAt( 9 + 3, display.charAt(3*9 + 7));
-        stringBuilder.setCharAt( 9 + 4, display.charAt(3*9 + 4));
-        stringBuilder.setCharAt( 9 + 5, display.charAt(3*9 + 1));
-        stringBuilder.setCharAt( 9 + 6, display.charAt(3*9 + 8));
-        stringBuilder.setCharAt( 9 + 7, display.charAt(3*9 + 5));
-        stringBuilder.setCharAt( 9 + 8, display.charAt(3*9 + 2));
-
-        stringBuilder.setCharAt( 3*9 + 0, display.charAt(4*9 + 6));
-        stringBuilder.setCharAt( 3*9 + 1, display.charAt(4*9 + 3));
-        stringBuilder.setCharAt( 3*9 + 2, display.charAt(4*9 + 0));
-        stringBuilder.setCharAt( 3*9 + 3, display.charAt(4*9 + 7));
-        stringBuilder.setCharAt( 3*9 + 4, display.charAt(4*9 + 4));
-        stringBuilder.setCharAt( 3*9 + 5, display.charAt(4*9 + 1));
-        stringBuilder.setCharAt( 3*9 + 6, display.charAt(4*9 + 8));
-        stringBuilder.setCharAt( 3*9 + 7, display.charAt(4*9 + 5));
-        stringBuilder.setCharAt( 3*9 + 8, display.charAt(4*9 + 2));
-
-        stringBuilder.setCharAt( 4*9 + 0, display.charAt(6));
-        stringBuilder.setCharAt( 4*9 + 1, display.charAt(3));
-        stringBuilder.setCharAt( 4*9 + 2, display.charAt(0));
-        stringBuilder.setCharAt( 4*9 + 3, display.charAt(7));
-        stringBuilder.setCharAt( 4*9 + 4, display.charAt(4));
-        stringBuilder.setCharAt( 4*9 + 5, display.charAt(1));
-        stringBuilder.setCharAt( 4*9 + 6, display.charAt(8));
-        stringBuilder.setCharAt( 4*9 + 7, display.charAt(5));
-        stringBuilder.setCharAt( 4*9 + 8, display.charAt(2));
-
-        display = stringBuilder.toString();
-
-//            for (int i = 0; i < 9; i++) {
-//                stringBuilder.setCharAt(0 * 9 + i, display.charAt(1 * 9 + i));
-//                stringBuilder.setCharAt(1 * 9 + i, display.charAt(3 * 9 + i));
-//                stringBuilder.setCharAt(3 * 9 + i, display.charAt(4 * 9 + i));
-//                stringBuilder.setCharAt(4 * 9 + i, display.charAt(0 * 9 + i));
-//            }
     }
 }
