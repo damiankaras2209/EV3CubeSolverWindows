83e4f93a9de15ef907e1c8dc35bbade90f39ad67
Damian Karaś <damiankaras2209@gmail.com>
2022-08-27 11:52:33 +0200

    add remote solving

--- src/me/damiankaras/ev3cubesolver/windows/NetworkData.java
+++ src/me/damiankaras/ev3cubesolver/windows/NetworkData.java
@@ -2,10 +2,15 @@ package me.damiankaras.ev3cubesolver.windows;
 
 import me.damiankaras.ev3cubesolver.windows.GUI.Log;
 
+import cs.min2phase.Search;
+
+
 public class NetworkData {
 
     public static final String DATATYPE_CUBE = "CB";
     public static final String DATATYPE_COMMAND = "CMD";
+    public static final String DATATYPE_SOLVE = "SLV";
+    public static final String DATATYPE_SOLUTION = "SOL";
     public static final String DATATYPE_LOG = "L";
 
     private static NetworkData instance = new NetworkData();
@@ -32,6 +37,26 @@ public class NetworkData {
         switch(prefix) {
             case DATATYPE_CUBE:
                 Main.queue.add(() -> Cube.getInstance().update(data));
+                break;
+            case DATATYPE_SOLVE:
+                Main.queue.add(() -> {
+                    Search search = new Search();
+
+                    int mask = 0;
+                    mask |= false ? Search.USE_SEPARATOR : 0;
+                    mask |= false ? Search.INVERSE_SOLUTION : 0;
+                    mask |= true ? Search.APPEND_LENGTH : 0;
+
+                    String out;
+
+                    System.out.println("Solving locally... ");
+                    out = search.solution(data, 21, 100, 0, mask);
+                    if (out.contains("Error")) {
+                        out = "error";
+                    }
+                    System.out.println("Solved ");
+                    Network.getInstance().send(DATATYPE_SOLUTION, out);
+                });
                 break;
             case DATATYPE_LOG:
                 Main.queue.add(() -> Main.log.append(data));
